ddcadddcf78482f240054305d13ec042c8f78f32
AlexChaos <the.airlight@gmail.com>
2022-06-09 14:48:31 +0300

    Добавляет функцию с проверками.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,33 @@
+/* Функция возвращающая случайное цело число из переданного диапазона с проверками */
+const isNumber = (randomNumber) => typeof randomNumber === 'number' || Number.isFinite(randomNumber) || !isNaN(randomNumber);
+
+const returnRandomNumber = (min, max) => {
+  if (max <= min) {
+    [min, max] = [max, min];
+  }
+  if (!isNumber(min) || !isNumber(max)) {
+    throw new TypeError('Params not right');
+  }
+  let randomNumber = Math.random() * -(max - min) + max;
+  /* Если число отрицательное, то заносим результат в модуль Math.abs и возвращаем положительным */
+  if (randomNumber < 0) {
+    randomNumber = Math.abs(randomNumber);
+  }
+  if (!isNumber(randomNumber)) {
+    throw new TypeError('Params not right');
+  }
+  return Math.round(randomNumber);
+};
+
+returnRandomNumber(0, 30);
+//console.log(returnRandomNumber([2], [5]));
+
 /* Функция, возвращающая случайное целое число из переданного диапазона включительно.
 диапазон может быть только положительный, включая ноль.
 А также придумайте, как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему.
 */
 
-const getRandomIntInclusive = (min, max) => {
+function getRandomIntInclusive(min, max) {
   if (min < 0 || max < 0) {
     return 'Error';
   }
@@ -11,9 +35,9 @@ const getRandomIntInclusive = (min, max) => {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-};
+}
 getRandomIntInclusive(3, 8);
-//console.log(getRandomIntInclusive(3, 8));
+//console.log(getRandomIntInclusive(2, 8));
 
 /*
   Функция для проверки максимальной длины строки.
@@ -25,7 +49,7 @@ const maxSrtLength = (str, maxLength) => {
     throw new TypeError('Не строка');
   }
 
-  return str.length < maxLength;
+  return str.length <= maxLength;
 };
 maxSrtLength('large string', 50);
 //console.log(maxSrtLength('large string', 50));
