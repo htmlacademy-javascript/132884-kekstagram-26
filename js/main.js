4699c8408881e99454448575038ae2a0295e56c0
AlexChaos <the.airlight@gmail.com>
2022-06-17 15:43:08 +0300

    Выполняет задание, больше деталей, исправляет ошибки.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,58 @@
-/*
-  В файле main.js на основе написанных по заданию ранее вспомогательных функций напишите необходимые функции для создания массива из 25 сгенерированных объектов. Каждый объект массива — описание фотографии, опубликованной пользователем.
-*/
+const IDENTIFICATOR = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+  '24',
+  '24',
+];
+
+const AVATAR = [
+  'img/avatar-1.svg',
+  'img/avatar-2.svg',
+  'img/avatar-3.svg',
+  'img/avatar-4.svg',
+  'img/avatar-5.svg',
+  'img/avatar-6.svg',
+  'img/avatar-7.svg',
+  'img/avatar-8.svg',
+  'img/avatar-9.svg',
+  'img/avatar-10.svg',
+  'img/avatar-11.svg',
+  'img/avatar-12.svg',
+  'img/avatar-13.svg',
+  'img/avatar-14.svg',
+  'img/avatar-15.svg',
+  'img/avatar-16.svg',
+  'img/avatar-17.svg',
+  'img/avatar-18.svg',
+  'img/avatar-19.svg',
+  'img/avatar-20.svg',
+  'img/avatar-21.svg',
+  'img/avatar-22.svg',
+  'img/avatar-23.svg',
+  'img/avatar-24.svg',
+  'img/avatar-25.svg',
+];
 
 const NAMES = [
   'Константин',
@@ -67,6 +119,91 @@ const MESSAGE = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
 
+const ADDRESS = [
+  'photos/photo-1.jpg',
+  'photos/photo-2.jpg',
+  'photos/photo-3.jpg',
+  'photos/photo-4.jpg',
+  'photos/photo-5.jpg',
+  'photos/photo-6.jpg',
+  'photos/photo-7.jpg',
+  'photos/photo-8.jpg',
+  'photos/photo-9.jpg',
+  'photos/photo-10.jpg',
+  'photos/photo-11.jpg',
+  'photos/photo-12.jpg',
+  'photos/photo-13.jpg',
+  'photos/photo-14.jpg',
+  'photos/photo-15.jpg',
+  'photos/photo-16.jpg',
+  'photos/photo-17.jpg',
+  'photos/photo-18.jpg',
+  'photos/photo-19.jpg',
+  'photos/photo-20.jpg',
+  'photos/photo-21.jpg',
+  'photos/photo-22.jpg',
+  'photos/photo-23.jpg',
+  'photos/photo-24.jpg',
+  'photos/photo-25.jpg',
+
+];
+
+const DESCRIPTION = [
+  'photo-1',
+  'photo-2',
+  'photo-3',
+  'photo-4',
+  'photo-5',
+  'photo-6',
+  'photos-7',
+  'photos-8',
+  'photos-9',
+  'photos-10',
+  'photos-11',
+  'photos-12',
+  'photos-13',
+  'photos-14',
+  'photos-15',
+  'photos-16',
+  'photos-17',
+  'photos-18',
+  'photos-19',
+  'photos-20',
+  'photos-21',
+  'photos-22',
+  'photos-23',
+  'photos-24',
+  'photos-25',
+];
+
+const LIKES = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+  '24',
+  '24',
+];
+
 const GENERATED_OBJECT = 25;
 
 const getRandomPositiveInteger = (min, max) => {
@@ -81,26 +218,15 @@ const getRandomPositiveInteger = (min, max) => {
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
 
 const createRandomObject = () => ({
-  name: `${getRandomArrayElement(NAMES)  } ${  getRandomArrayElement(SURNAMES)  } ${  getRandomArrayElement(MESSAGE)}`,
+  id: `${getRandomArrayElement(IDENTIFICATOR)  }`,
+  avatar: `${getRandomArrayElement(AVATAR)  }`,
+  name: `${getRandomArrayElement(NAMES)  } ${  getRandomArrayElement(SURNAMES)}`,
+  url: `${getRandomArrayElement(ADDRESS) }`,
+  description: `${getRandomArrayElement(DESCRIPTION) }`,
+  message: `${getRandomArrayElement(MESSAGE) }`,
+  likes: `${getRandomArrayElement(LIKES) }`,
 });
 
 const generateRandomObject = Array.from({length: GENERATED_OBJECT}, createRandomObject);
 
 //console.log(generateRandomObject);
-
-/*
-const myObject = {
-  id: 1,
-  url: 'photos/{{photo-1}}.jpg',
-  description: 'photo1',
-  likes: 2,
-  comments: [
-    {
-      id: 135,
-      avatar: 'img/avatar-6.svg',
-      message: 'В целом всё неплохо. Но не всё.',
-      name: 'Артём',
-    }
-  ]
-};
-*/
