a0ce617c0c1ae0d3c8aace85fdc7c17be31dfa8f
AlexChaos <the.airlight@gmail.com>
2022-07-04 00:17:30 +0300

    Не вводятся с клавиатуры текс в поля input и textarea в форме.

--- js/form.js
+++ js/form.js
@@ -1,11 +1,28 @@
 import {initPopup} from './popup.js';
 
 const uploadOverlayElement = document.querySelector('.img-upload__overlay');
+const form = document.querySelector('.img-upload__form');
 
 const {openPopup} = initPopup(uploadOverlayElement);
 
 const showUploadForm = () => {
   openPopup();
+};
+
+const pristine = new Pristine((form), {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper',
+});
+
+window.onload = () => {
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const isValid = pristine.validate();
+    if (isValid) {
+      return true;
+    }
+  });
 };
 
 export {showUploadForm};
